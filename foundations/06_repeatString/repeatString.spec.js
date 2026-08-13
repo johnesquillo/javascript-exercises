7134ad2678a6985b2dbdb2615a8038f3a13e818b
johnesquillo <johnesquillo286@gmail.com>
2026-08-13 17:38:55 +0800

    Done with this task

--- foundations/06_repeatString/repeatString.spec.js
+++ foundations/06_repeatString/repeatString.spec.js
@@ -14,14 +14,12 @@ describe('repeatString', () => {
     expect(repeatString('bye', 0)).toEqual('');
   });
   test.skip('handles any non-negative integer for repeat count', function () {
-    /* This test ensures your function is "generic", meaning the previous
-      tests were not completed by hardcoding values for their respective numbers */
+    
     const number = Math.floor(Math.random() * 1000);
     expect(repeatString('hey', number)).toBe('hey'.repeat(number));
   });
   test.skip('does not use the built-in String repeat method', () => {
-    /* Even though there is a built-in String repeat method,
-      in this exercise specifically, we want you to practise using loops */
+    
     jest.spyOn(String.prototype, 'repeat').mockName('Built-in String repeat method');
     repeatString("don't use the built-in repeat method!", 1);
     expect(String.prototype.repeat).not.toHaveBeenCalled();
